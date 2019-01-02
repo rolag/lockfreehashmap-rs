1d85d68d1d1dd1d067f31630fee8290d4ac948de
travis-ci <travis@travis-ci.org>
2019-01-02 23:23:31 +0000

    Update developer docs for commit 97883c2

---
+++ master/implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lockfreehashmap"] = [{text:"impl&lt;'guard, 'v, K, V, S&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"lockfreehashmap/struct.Keys.html\" title=\"struct lockfreehashmap::Keys\">Keys</a>&lt;'guard, 'v, K, V, S&gt;",synthetic:false,types:["lockfreehashmap::Keys"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
